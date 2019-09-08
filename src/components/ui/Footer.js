dba383d60825c11052bf8996f5b7e7f9bc1bd386
b-ubben <brackben@gmail.com>
2019-09-07 19:47:51 -0700

    dumb one letter change in footer for consistency

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -17,7 +17,7 @@ const FooterCopy = styled.p`
 const Footer = () => (
   <FooterContainer>
     <FooterCopy>
-      MyAnime.Page &copy; 2019
+      myAnime.Page &copy; 2019
       <br />
       Questions or Comments? Tweet <a href="https://twitter.com/_ubben" target="_blank" rel="noopener noreferrer">@_ubben</a></FooterCopy>
   </FooterContainer>
