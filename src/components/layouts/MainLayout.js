8ddf3fdb59734761f816d3398c377a7b5835ed07
b-ubben <brackben@gmail.com>
2019-09-12 18:29:09 -0700

    moved twitter card meta to index.html

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -34,11 +34,6 @@ const MainLayout = props => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="description" content={description || 'myAnime.Page | Anime for you!'} />
                 <meta name="keywords" content={keywords || 'anime, anime database, myanime, myanimepage'} />
-                <meta name="twitter:card" content="summary" />
-                <meta name="twitter:title" content="myAnime.Page" />
-                <meta name="twitter:description" content="Anime for you! Powered by Kitsu.io API" />
-                <meta name="twitter:image" content="https://images.ubben.co/myanimepage-twitter-card.png" />
-                <meta name="twitter:image:alt" content="Index page of myAnime.Page" />
 
                 <title>{title || 'myAnime.Page | Anime for You!'}</title>
             </Helmet>
